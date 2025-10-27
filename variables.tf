b584a05f4a27c1182af7ca82e422a5d457e7bf76
bayazidakm-sudo <bayazidakm@gmail.com>
2025-10-27 22:59:14 +0100

    Refactor variables with types and new entries

--- variables.tf
+++ variables.tf
@@ -1,65 +1,83 @@
 variable "resource_group_name" {
   description = "Name of the resource group"
+  type        = string
   default     = "webapp-rg"
 }
 
 variable "location" {
-  description = "Azure region"
+  description = "Azure region for resources"
+  type        = string
   default     = "eastus"
 }
 
+variable "environment" {
+  description = "Environment name for tagging"
+  type        = string
+  default     = "production"
+}
+
+variable "allowed_ssh_ips" {
+  description = "List of IP addresses allowed to SSH"
+  type        = list(string)
+  default     = []  # Add your trusted IPs here
+}
+
 variable "vm_count" {
-  description = "Number of VMs to create"
+  description = "Number of VMs to deploy"
+  type        = number
   default     = 2
 }
 
 variable "vm_size" {
   description = "Size of the VMs"
+  type        = string
   default     = "Standard_B2s"
 }
 
 variable "admin_username" {
   description = "Admin username for VMs"
-  default     = "webadmin"
+  type        = string
+  default     = "azureuser"
 }
 
 variable "admin_password" {
   description = "Admin password for VMs"
+  type        = string
   sensitive   = true
 }
 
-variable "trusted_ips" {
-  description = "List of trusted IP addresses for SSH/RDP access"
-  type        = list(string)
-  default     = ["0.0.0.0/0"]  # Replace with actual trusted IPs
-}
-
-variable "db_name" {
-  description = "Name of the Azure SQL Database"
-  default     = "webapp-db"
-}
-
-variable "db_admin_login" {
-  description = "Database administrator login"
+variable "db_admin_username" {
+  description = "Database admin username"
+  type        = string
   default     = "sqladmin"
 }
 
 variable "db_admin_password" {
-  description = "Database administrator password"
+  description = "Database admin password"
+  type        = string
   sensitive   = true
 }
 
-variable "environment" {
-  description = "Environment name for tagging"
-  default     = "production"
+variable "db_sku" {
+  description = "SKU for Azure SQL Database"
+  type        = string
+  default     = "Basic"
+}
+
+variable "enable_monitoring" {
+  description = "Enable Azure Monitor and App Insights"
+  type        = bool
+  default     = true
 }
 
 variable "log_retention_days" {
   description = "Number of days to retain logs"
+  type        = number
   default     = 30
 }
 
-variable "daily_quota_gb" {
-  description = "Daily data volume quota for Application Insights in GB"
-  default     = 5
+variable "daily_log_quota_gb" {
+  description = "Daily quota for logs in GB"
+  type        = number
+  default     = 1
 }
